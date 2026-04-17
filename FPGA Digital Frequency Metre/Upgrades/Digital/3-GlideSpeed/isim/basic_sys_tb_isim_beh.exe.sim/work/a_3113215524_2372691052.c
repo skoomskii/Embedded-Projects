73e9e021f66a09a1f3e5062ff2e9d6b79339f10e
skoomskii <161514933+skoomskii@users.noreply.github.com>
2026-04-17 16:34:53 -0400

    Add files via upload

---
+++ FPGA Digital Frequency Metre/Upgrades/Digital/3-GlideSpeed/isim/basic_sys_tb_isim_beh.exe.sim/work/a_3113215524_2372691052.c
@@ -0,0 +1,256 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x6dd86d03 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/.Xilinx/IDP/basic_sys_tb.vhd";
+
+
+
+static void work_a_3113215524_2372691052_p_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    int64 t7;
+    int64 t8;
+
+LAB0:    t1 = (t0 + 1848U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(87, ng0);
+    t2 = (t0 + 2368);
+    t3 = (t2 + 32U);
+    t4 = *((char **)t3);
+    t5 = (t4 + 40U);
+    t6 = *((char **)t5);
+    *((unsigned char *)t6) = (unsigned char)2;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(88, ng0);
+    t2 = (t0 + 1224U);
+    t3 = *((char **)t2);
+    t7 = *((int64 *)t3);
+    t8 = (t7 / 2);
+    t2 = (t0 + 1748);
+    xsi_process_wait(t2, t8);
+
+LAB6:    *((char **)t1) = &&LAB7;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(89, ng0);
+    t2 = (t0 + 2368);
+    t3 = (t2 + 32U);
+    t4 = *((char **)t3);
+    t5 = (t4 + 40U);
+    t6 = *((char **)t5);
+    *((unsigned char *)t6) = (unsigned char)3;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(90, ng0);
+    t2 = (t0 + 1224U);
+    t3 = *((char **)t2);
+    t7 = *((int64 *)t3);
+    t8 = (t7 / 2);
+    t2 = (t0 + 1748);
+    xsi_process_wait(t2, t8);
+
+LAB10:    *((char **)t1) = &&LAB11;
+    goto LAB1;
+
+LAB5:    goto LAB4;
+
+LAB7:    goto LAB5;
+
+LAB8:    goto LAB2;
+
+LAB9:    goto LAB8;
+
+LAB11:    goto LAB9;
+
+}
+
+static void work_a_3113215524_2372691052_p_1(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    int64 t7;
+    int64 t8;
+
+LAB0:    t1 = (t0 + 1992U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(96, ng0);
+    t2 = (t0 + 2404);
+    t3 = (t2 + 32U);
+    t4 = *((char **)t3);
+    t5 = (t4 + 40U);
+    t6 = *((char **)t5);
+    *((unsigned char *)t6) = (unsigned char)2;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(97, ng0);
+    t2 = (t0 + 1292U);
+    t3 = *((char **)t2);
+    t7 = *((int64 *)t3);
+    t8 = (t7 / 2);
+    t2 = (t0 + 1892);
+    xsi_process_wait(t2, t8);
+
+LAB6:    *((char **)t1) = &&LAB7;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(98, ng0);
+    t2 = (t0 + 2404);
+    t3 = (t2 + 32U);
+    t4 = *((char **)t3);
+    t5 = (t4 + 40U);
+    t6 = *((char **)t5);
+    *((unsigned char *)t6) = (unsigned char)3;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(99, ng0);
+    t2 = (t0 + 1292U);
+    t3 = *((char **)t2);
+    t7 = *((int64 *)t3);
+    t8 = (t7 / 2);
+    t2 = (t0 + 1892);
+    xsi_process_wait(t2, t8);
+
+LAB10:    *((char **)t1) = &&LAB11;
+    goto LAB1;
+
+LAB5:    goto LAB4;
+
+LAB7:    goto LAB5;
+
+LAB8:    goto LAB2;
+
+LAB9:    goto LAB8;
+
+LAB11:    goto LAB9;
+
+}
+
+static void work_a_3113215524_2372691052_p_2(char *t0)
+{
+    char *t1;
+    char *t2;
+    int64 t3;
+    char *t4;
+    int64 t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    t1 = (t0 + 2136U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(106, ng0);
+    t3 = (100 * 1000LL);
+    t2 = (t0 + 2036);
+    xsi_process_wait(t2, t3);
+
+LAB6:    *((char **)t1) = &&LAB7;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(107, ng0);
+    t2 = (t0 + 1224U);
+    t4 = *((char **)t2);
+    t3 = *((int64 *)t4);
+    t5 = (t3 * 10);
+    t2 = (t0 + 2036);
+    xsi_process_wait(t2, t5);
+
+LAB10:    *((char **)t1) = &&LAB11;
+    goto LAB1;
+
+LAB5:    goto LAB4;
+
+LAB7:    goto LAB5;
+
+LAB8:    xsi_set_current_line(110, ng0);
+    t2 = (t0 + 2440);
+    t4 = (t2 + 32U);
+    t6 = *((char **)t4);
+    t7 = (t6 + 40U);
+    t8 = *((char **)t7);
+    *((unsigned char *)t8) = (unsigned char)3;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(111, ng0);
+    t3 = (10 * 1000LL);
+    t2 = (t0 + 2036);
+    xsi_process_wait(t2, t3);
+
+LAB14:    *((char **)t1) = &&LAB15;
+    goto LAB1;
+
+LAB9:    goto LAB8;
+
+LAB11:    goto LAB9;
+
+LAB12:    xsi_set_current_line(112, ng0);
+    t2 = (t0 + 2440);
+    t4 = (t2 + 32U);
+    t6 = *((char **)t4);
+    t7 = (t6 + 40U);
+    t8 = *((char **)t7);
+    *((unsigned char *)t8) = (unsigned char)2;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(114, ng0);
+
+LAB18:    *((char **)t1) = &&LAB19;
+    goto LAB1;
+
+LAB13:    goto LAB12;
+
+LAB15:    goto LAB13;
+
+LAB16:    goto LAB2;
+
+LAB17:    goto LAB16;
+
+LAB19:    goto LAB17;
+
+}
+
+
+extern void work_a_3113215524_2372691052_init()
+{
+	static char *pe[] = {(void *)work_a_3113215524_2372691052_p_0,(void *)work_a_3113215524_2372691052_p_1,(void *)work_a_3113215524_2372691052_p_2};
+	xsi_register_didat("work_a_3113215524_2372691052", "isim/basic_sys_tb_isim_beh.exe.sim/work/a_3113215524_2372691052.didat");
+	xsi_register_executes(pe);
+}
---
+++ FPGA Digital Frequency Metre/Upgrades/Digital/3-GlideSpeed/isim/basic_sys_tb_isim_beh.exe.sim/work/basic_sys_tb_isim_beh.exe_main.c
@@ -0,0 +1,62 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+#include "xsi.h"
+
+struct XSI_INFO xsi_info;
+
+char *IEEE_P_2592010699;
+char *STD_STANDARD;
+char *IEEE_P_3499444699;
+char *IEEE_P_3620187407;
+char *UNISIM_P_0947159679;
+
+
+int main(int argc, char **argv)
+{
+    xsi_init_design(argc, argv);
+    xsi_register_info(&xsi_info);
+
+    xsi_register_min_prec_unit(-12);
+    ieee_p_2592010699_init();
+    ieee_p_3499444699_init();
+    ieee_p_3620187407_init();
+    unisim_p_0947159679_init();
+    work_a_0032968822_3212880686_init();
+    work_a_0146954563_3212880686_init();
+    unisim_a_2988077518_2751630626_init();
+    work_a_1907293928_3212880686_init();
+    work_a_2385712763_3212880686_init();
+    work_a_3928147440_3212880686_init();
+    work_a_0775787291_3212880686_init();
+    work_a_4262033764_3212880686_init();
+    work_a_3501830454_3212880686_init();
+    work_a_0068816927_3212880686_init();
+    work_a_3565957405_3212880686_init();
+    work_a_1794720123_3212880686_init();
+    work_a_1913114936_3212880686_init();
+    work_a_3687991861_3212880686_init();
+    work_a_3113215524_2372691052_init();
+
+
+    xsi_register_tops("work_a_3113215524_2372691052");
+
+    IEEE_P_2592010699 = xsi_get_engine_memory("ieee_p_2592010699");
+    xsi_register_ieee_std_logic_1164(IEEE_P_2592010699);
+    STD_STANDARD = xsi_get_engine_memory("std_standard");
+    IEEE_P_3499444699 = xsi_get_engine_memory("ieee_p_3499444699");
+    IEEE_P_3620187407 = xsi_get_engine_memory("ieee_p_3620187407");
+    UNISIM_P_0947159679 = xsi_get_engine_memory("unisim_p_0947159679");
+
+    return xsi_run_simulation(argc, argv);
+
+}
